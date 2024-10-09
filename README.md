66e0d37265a0255693948a1d97aea9a9375fae39
Louise Dupont <louisesdupont@gmail.com>
2024-10-09 14:53:39 -0400

    add spotify api access

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # How to run
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). To run, clone the repository, then run `npm start`.
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). To run, clone the repository, then run `npm start`. To access the Spotify API, you must either pass a `REACT_APP_SPOTIFY_CLIENT_ID` environment variable with your Spotify client ID through the command line, or create a `.env` file in root containing this value (see: https://create-react-app.dev/docs/adding-custom-environment-variables/). This is not secure if publishing a build of the app.
 
 # Attributions
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token"
+  const scopes = [
+    "user-top-read"
+  ]
+
+  const [token, setToken] = useState("")
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+}, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchTopItems = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/me/top/artists", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+    })
+  
+    setArtists(data.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Spotify React</h1>
+                {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join("%20")}&response_type=${RESPONSE_TYPE}`}>Login
+                        to Spotify</a>
+                    : <button onClick={logout}>Logout</button>}
+
+    <button onClick={searchTopItems}>
+        Get top artists.
+    </button>
+
+    {renderArtists()}
       </header>
     </div>
   );
